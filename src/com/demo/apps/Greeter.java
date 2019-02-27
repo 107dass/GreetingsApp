745da92353523e51a8a4869f6355a2b5d9db8c70
dthulasi <dthulasi@ford.com>
2019-02-27 20:16:25 +0530

    Main class & gitignore files

--- src/com/demo/apps/Greeter.java
+++ src/com/demo/apps/Greeter.java
@@ -1,5 +1,18 @@
 package com.demo.apps;
 
+/**
+ * 
+ * @author DTHULASI
+ *
+ */
 public class Greeter {
 
+ public String greetGuest() {
+	 return "Hello Guest";
+ }
+ 
+ public String greetUser(String name) {
+	 return "Hello "+name;
+ }
+ 
 }
---
+++ src/com/demo/apps/GreeterApplication.java
@@ -0,0 +1,10 @@
+package com.demo.apps;
+
+public class GreeterApplication {
+
+	public static void main(String args[]) {
+		Greeter greet = new Greeter();
+		greet.greetUser("FS Team");
+	}
+}
+ 
